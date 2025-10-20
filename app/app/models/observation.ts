982f1e47bc6592940cd4f77d53bf5b23df9f43a4
Michael Starmer <michael.starmer@gmail.com>
2025-10-20 19:26:33 +0200

    Added model + migration Observation

---
+++ app/app/models/observation.ts
@@ -0,0 +1,33 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Observation extends BaseModel {
+  
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare title: string
+
+  @column()
+  declare description?: string
+
+  @column.dateTime()
+  declare occurredAt: DateTime
+
+  @column.dateTime()
+  declare reportedAt?: DateTime
+
+  @column() declare country: string
+  @column() declare latitude: number
+  @column() declare longitude: number
+  @column() declare verificationLevel: number
+  @column() declare category?: string
+  @column() declare locAccuracyM?: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ app/database/migrations/1760978493162_create_observations_table.ts
@@ -0,0 +1,26 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'observations'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title').notNullable()
+      table.text('description').defaultTo(null)
+      table.timestamp('occurred_at', { useTz: true }).notNullable()
+      table.timestamp('reported_at', { useTz: true }).defaultTo(this.now())
+      table.string('country', 2).notNullable() // NO, SE, DK, etc.
+      table.decimal('latitude', 10, 7).notNullable()
+      table.decimal('longitude', 10, 7).notNullable()
+      table.integer('verification_level').notNullable().defaultTo(1) // 1..5
+      table.string('category').defaultTo('uap') // uap|drone|unknown
+      table.integer('loc_accuracy_m').defaultTo(null)
+      table.timestamps(true) // created_at, updated_at with tz
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
