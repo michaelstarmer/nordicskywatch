9e10866b56a3b5d6a76d9f8c5bdef8a7e486119c
Michael Starmer <michael.starmer@gmail.com>
2025-11-11 19:11:09 +0100

    Add login view. Add theme-color meta attribute to layout head.

--- app/start/routes.ts
+++ app/start/routes.ts
@@ -11,11 +11,15 @@ import router from '@adonisjs/core/services/router'
 import ObservationsController from '#controllers/observations_controller'
 import ArticlesController from '#controllers/articles_controller'
 import MainsController from '#controllers/main_controller'
+import AdminController from '#controllers/admin_controller'
 
 // router.on('/').render('pages/home')
 router.get('/', [MainsController, 'index'])
 router.get('observasjoner', [ObservationsController, 'index'])
 router.on('innsyn').render('pages/innsyn')
 router.on('arkiv').render('pages/arkiv')
+
+router.get('login', [AdminController, 'show_login'])
+
 router.get('artikler', [ArticlesController, 'index'])
 router.get('artikler/:slug', [ArticlesController, 'show'])
