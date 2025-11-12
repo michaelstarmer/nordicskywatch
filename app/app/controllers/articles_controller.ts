6075834932b85a0897e087e8698fb1d756b8f3d8
Michael Starmer <michael.starmer@gmail.com>
2025-11-12 22:19:02 +0100

    fix middleware. add article dashboard control

--- app/app/controllers/articles_controller.ts
+++ app/app/controllers/articles_controller.ts
@@ -2,21 +2,20 @@ import type { HttpContext } from '@adonisjs/core/http'
 import Article from '#models/article'
 
 export default class ArticlesController {
-    async index({response, view}: HttpContext)
-    {
+    async index({ response, view }: HttpContext) {
         const articles = await Article.all();
 
         return view.render('pages/article')
     }
 
-    async show({ response, view, request }: HttpContext)
-    {
+    async show({ response, view, request }: HttpContext) {
         const slug = request.param('slug');
         try {
             const article = await Article.findByOrFail('slug', slug);
-            return view.render('pages/article', {article});
+            console.log(`article slug: /artikler/${article.slug}`)
+            return view.render('pages/article', { article });
         } catch (error) {
-            return response.json({success: false, error})
+            return response.json({ success: false, error })
         }
     }
 }
